ecf286fc5115832730ae753d4adac88a2c79c808
Mohamed Ouaammou <moouaamm@e2r6p9.1337.ma>
2025-01-22 21:19:58 +0100

    add allow all cors ...

--- backend/authentication/simplemiddleware.py
+++ backend/authentication/simplemiddleware.py
@@ -3,8 +3,8 @@ def simple_middleware(get_response):
     def middleware(request):
         response = get_response(request)
         response["Access-Control-Allow-Origin"] = "https://ft-transcendance-1.onrender.com"
-        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization, Pragma, Cache-Control" 
+        response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS, PATCH, HEAD, CONNECT, TRACE, COPY, LOCK, MKCOL, MOVE, PROPFIND, PROPPATCH, SEARCH, UNLOCK"
+        response["Access-Control-Allow-Headers"] = "Content-Type, Authorization, Pragma, Cache-Control, If-Modified-Since, If-None-Match, X-Requested-With, X-CSRFToken, X-Frame-Options, X-Access-Token, X-Refresh-Token, X-Username, X-Password, Expires"
         response["Access-Control-Allow-Credentials"] = "true"
         return response
     return middleware
